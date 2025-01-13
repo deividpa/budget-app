b6d9ab87623b40e74239aa4164d1b999460550ba
= <deividpa022@gmail.com>
2025-01-13 00:03:27 -0500

    feat: add hidden input for user creation action in Intro component

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -19,6 +19,7 @@ const Intro = () => {
             aria-label="Your Name" 
             autoComplete="given-name" 
           />
+          <input type="hidden" name="_action" value="createUser" />
           <button type="submit" className='btn btn--dark'>
             <span>Create Account</span>
             <UserPlusIcon width={20} />
