92eeb5576d440fd918146f30b6778ef5f5a92deb
= <deividpa022@gmail.com>
2025-01-13 09:35:37 -0500

    feat: enhance AddBudgetForm to reset and focus on input after submission

--- src/components/AddBudgetForm.jsx
+++ src/components/AddBudgetForm.jsx
@@ -1,11 +1,26 @@
 import { CurrencyDollarIcon } from "@heroicons/react/24/solid";
-import { Form } from "react-router-dom";
+import { useEffect, useRef } from "react";
+import { Form, useFetcher } from "react-router-dom";
 
 const AddBudgetForm = () => {
+
+  const fetcher = useFetcher();
+  const isSubmitting = fetcher.state === 'submitting';
+
+  const formRef = useRef();
+  const focusRef = useRef();
+
+  useEffect(() => {
+    if (!isSubmitting) {
+      formRef.current.reset();
+      focusRef.current.focus();
+    }
+  }, [isSubmitting]);
+
   return (
     <div className='form-wrapper'>
       <h2 className="h3">Create Budget</h2>
-      <Form method="post" className='grid-sm'>
+      <fetcher.Form method="post" className='grid-sm' ref={formRef}>
         <div className="grid-xs">
           <label htmlFor="newBudget">Budget Name</label>
           <input 
@@ -16,7 +31,7 @@ const AddBudgetForm = () => {
             autoComplete="off"
             aria-label="Budget Name"
             required
-            autoFocus
+            ref={focusRef}
           />
         </div>
         <div className="grid-xs">
@@ -32,12 +47,14 @@ const AddBudgetForm = () => {
             inputMode="decimal"
           />
           <input type="hidden" name="_action" value="createBudget" />
-          <button type="submit" className="btn btn--dark">
-            <span>Create Budget</span>
-            <CurrencyDollarIcon width={20} />
+          <button type="submit" className="btn btn--dark" disabled={isSubmitting}>
+            {isSubmitting 
+              ? <span>Submitting...</span> 
+              : <><span>Create Budget</span> <CurrencyDollarIcon width={20} /></>
+            }
           </button>
         </div>
-      </Form>
+      </fetcher.Form>
     </div>
   )
 }
