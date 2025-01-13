df1c24677b9c1dd36455adc010bfdc04de958db9
= <deividpa022@gmail.com>
2025-01-12 20:11:27 -0500

    feat: add deleteData function to remove data from local storage

--- helpers.js
+++ helpers.js
@@ -2,3 +2,8 @@
 export const fetchData = (key) => {
     return JSON.parse(localStorage.getItem(key));
 }
+
+// Detelete data from local storage
+export const deleteData = ({ key }) => {
+    localStorage.removeItem(key);
+}
