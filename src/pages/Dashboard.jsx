5eadad3fa431e5a736d615c097e25a4ea4eecc47
= <deividpa022@gmail.com>
2025-01-12 21:22:35 -0500

    feat: integrate AddBudgetForm and display user budgets in Dashboard

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,5 @@
 import Intro from "../components/Intro";
+import AddBudgetForm from "../components/AddBudgetForm";
 
 import { useLoaderData } from "react-router-dom";
 import { fetchData } from "../../helpers";
@@ -7,7 +8,8 @@ import { toast } from "react-toastify";
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
-  return { userName };
+  const budgets = fetchData("budgets");
+  return { userName, budgets };
 }
 
 // action
@@ -24,11 +26,23 @@ export async function dashboardAction({ request }) {
 }
 
 const Dashboard = () => {
-  const { userName } = useLoaderData();
+  const { userName, budgets } = useLoaderData();
 
   return (
     <>
-      {userName ? (<p>{userName}</p>) : <Intro />}
+      {userName ? (
+        <div className="dashboard">
+          <h1>Welcome back, <span className="accent">{userName}</span></h1>
+          <div className="grid-sm">
+            {/* {budgets ? () : ()} */}
+            <div className="grid-lg">
+              <div className="flex-lg">
+                <AddBudgetForm />
+              </div>
+            </div>
+          </div>
+        </div>
+      ) : <Intro />}
     </>
   );
 };
