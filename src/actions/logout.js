ff185a6109bb94eacee2bbe29d8bd7877e08d2e1
= <deividpa022@gmail.com>
2025-01-15 09:34:32 -0500

    feat: enhance logoutAction to delete user-related budgets and expenses

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -5,6 +5,8 @@ import { toast } from "react-toastify";
 export async function logoutAction() {
   // delete the user
   deleteData({ key: "userName" });
+  deleteData({ key: "budgets" });
+  deleteData({ key: "expenses" });
   toast.success("You've deleted your account!");
   // return redirect
   return redirect("/");
