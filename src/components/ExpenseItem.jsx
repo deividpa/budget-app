5a3d17c5b158effc5e10d15ca470b7bb710c6aa3
= <deividpa022@gmail.com>
2025-01-16 20:51:42 -0500

    feat: enhance ExpenseItem component to link to associated budget details

--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,25 @@
 import PropTypes from 'prop-types';
-import { formatCurrency, formatDate } from '../../helpers';
+import { formatCurrency, formatDate, getAllMatchingBudgets } from '../../helpers';
+import { Link } from 'react-router-dom';
 
 const ExpenseItem = ({expense}) => {
 
-  const {name, amount, createdAt} = expense;
+  const { name, amount,  createdAt} = expense;
+
+  const budget = getAllMatchingBudgets({
+    category: "budgets",
+    key: "id",
+    value: expense.budgetId,
+  })[0];
+
+  console.log("Budget: ", budget)
 
   return (
     <>
       <td>{name}</td>
       <td>{formatCurrency(amount)}</td>
       <td>{formatDate(createdAt)}</td>
+      <td><Link to={`/budget/${budget.id}`} style={{"--accent": budget.color}}>{budget.name}</Link></td>
     </>
   )
 }
