ea29599e89507bbc55250b14ee1cc0c053467d3b
= <deividpa022@gmail.com>
2025-01-17 00:01:42 -0500

    feat: update logout action to use deleteItem for removing user data

--- src/actions/logout.js
+++ src/actions/logout.js
@@ -1,12 +1,12 @@
 import { redirect } from "react-router-dom";
-import { deleteData } from "../../helpers";
+import { deleteItem } from "../../helpers";
 import { toast } from "react-toastify";
 
 export async function logoutAction() {
   // delete the user
-  deleteData({ key: "userName" });
-  deleteData({ key: "budgets" });
-  deleteData({ key: "expenses" });
+  deleteItem({ key: "userName" });
+  deleteItem({ key: "budgets" });
+  deleteItem({ key: "expenses" });
   toast.success("You've deleted your account!");
   // return redirect
   return redirect("/");
