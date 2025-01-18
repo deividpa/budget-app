e9725f6daeb6ca3f680771793743df8cee6d0ca9
= <deividpa022@gmail.com>
2025-01-17 23:44:46 -0500

    feat: add delete functionality to BudgetItem component with confirmation prompt

--- src/components/BudgetItem.jsx
+++ src/components/BudgetItem.jsx
@@ -1,7 +1,10 @@
 import PropTypes from 'prop-types';
 import { calculateSpentByBudget, formatCurrency, formatPercentage } from '../../helpers';
+import { Form, Link } from 'react-router-dom';
+import { BanknotesIcon } from '@heroicons/react/24/outline';
+import { TrashIcon } from '@heroicons/react/24/solid';
 
-const BudgetItem = ({budget}) => {
+const BudgetItem = ({budget, showDelete = false}) => {
 
   const {id, name, amount, color} = budget;
   const spent = calculateSpentByBudget(id);
@@ -24,6 +27,32 @@ const BudgetItem = ({budget}) => {
         <small>{formatCurrency(spent)} spent</small>
         <small>{formatCurrency(amount - spent)} remaining</small>
       </div>
+      {
+        showDelete ? (
+          <div className="flex-sm">
+            <Form
+              method="post"
+              action="delete"
+              onSubmit={(event) => {
+                if (!confirm(`Are you sure you want to delete ${name}?`)) {
+                  event.preventDefault();
+                }
+             }}
+            >
+              <button type="submit" className="btn">
+                <span>Delete Budget</span>
+                <TrashIcon width={20} />
+              </button>
+            </Form>
+          </div>
+        ) : (
+          <div className="flex-sm">
+            <Link to={`budget/${id}`} className="btn">
+              View Details <BanknotesIcon width={20} />
+            </Link>
+          </div>
+        )
+      }
     </div>
   )
 }
@@ -34,7 +63,8 @@ BudgetItem.propTypes = {
     name: PropTypes.string.isRequired,
     amount: PropTypes.number.isRequired,
     color: PropTypes.string.isRequired
-  }).isRequired
+  }).isRequired,
+  showDelete: PropTypes.bool
 };
 
 export default BudgetItem
